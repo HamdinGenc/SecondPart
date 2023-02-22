6b8007cf2d544d06a7baf220fde9e4067b9a5e59
hamdin <hamdingenc1989@gmail.com>
2023-02-21 16:31:52 -0800

    Second Part Commit

---
+++ Company.java
@@ -0,0 +1,17 @@
+package GitProject01;
+
+public class Company {
+    public static void main(String[] args) {
+            //write your code here.
+
+            System.out.println("I am Groot!!!");
+            System.out.println("I am Groot!!!");
+            System.out.println("I am Groot!!!");
+
+        System.out.println("I am correct code");
+        System.out.println("i am not confused!");
+
+    }
+
+
+    }
---
+++ SecondPart.java
@@ -0,0 +1,8 @@
+package GitProject01;
+
+public class SecondPart {
+    public static void main(String[] args) {
+
+        System.out.println("It is new day");
+    }
+}
